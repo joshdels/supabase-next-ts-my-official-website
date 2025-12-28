6f746ce07d026b8eef1475daafcb4bfe12470b1e
joshdels <assistantgisjosh@gmail.com>
2025-12-28 19:22:39 +0800

    fix: mail flex issues

--- src/components/layout/MailLayout.tsx
+++ src/components/layout/MailLayout.tsx
@@ -5,10 +5,10 @@ export default function MailLayout() {
   return (
     <>
       <div className="flex flex-col xl:flex-row px-5 sm:px-5 md:px-20 lg:px-20 2xl:px-40 pt-10 pb-14 mt-15 bg-gray-50 gap-10">
-        <div className="flex-1">
+        <div className="flex-1 overflow-hidden">
           <DemoMap />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 overflow-hidden">
           <MailContent />
         </div>
       </div>
